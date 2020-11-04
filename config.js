b20a99c9d79beaeb14258e18ff03d7fa90d78f3e
Tillman, Amadia Marie <atillman36@gatech.edu>
2020-11-04 00:49:35 -0500

    Bare Minimum
    Still needs tweeking, but bot will now recommend a place similar to the one mentioned

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         'oFXGlOG6Fr2QlDBzXgLVkIeMY',
+  consumer_secret:      'p42eQC9S7yYsjLh0OQ2glMsVngbOjbG2Hw2V9jyhDN1kGzvIar',
+  access_token:         '1320210087349243904-06r9Wlh5asL3sYdmlF9f6tvR8mIO9Q',
+  access_token_secret:  '6Q65cxuZh7GZYmI7YStdGyxBkRcSVEY0wRGsgsRUFXBbg'
 }
--- get.js
+++ get.js
@@ -3,9 +3,18 @@ Search for a post whose user is likely interested in Atlanta attractions.
 Try again if the search does not produce a post or if the post contains content we want to avoid.
 Use the information from the post to respond with information about Atlanta attractions.
 */
-
+var debug = true;
 let Twit = require('twit');
 let T = new Twit(require('./config.js'));
+
+// Yelp API
+let yelp = require('yelp-fusion');
+let YelpAPIKey = yelp.client('bbZfR28tDLZ5JdiU4mJ1LojbOaVcOJB-0ZvUO8bLWnzObq4yNEBzEnsXFad2yNwUbbDJyhxO7xMSIcANFohw9aTN8xRDTZfm2HIRNLq4nZnugUA6u9BWrIKnnkGXX3Yx');
+var searchRequest = {
+    location: "Atlanta",
+    radius : 3000,
+    limit: 10,
+};
 
 //Creates FocusPost object to be updated by the postSearch function.
 function FocusPost(attraction, userId, postId, postIdStr) {
@@ -18,10 +27,29 @@ let focusPost = new FocusPost();
 
 
 //Creates params for get function
-let queriesArray = ["Aquarium", "Falcons", "Mercedes Benz stadium", "World of Coke", "World of Coca Cola", 
+let queriesArray = ["Aquarium", "Falcons", "Mercedes Benz stadium", "World of Coke", "World of Coca Cola",
 "Centennial Olympic Park", "Botanical Gardens", "Zoo", "High Museum of Art", "CNN Center", "Fernbank Museum", "Six Flags",
 "Fox Theatre", "Netherworld", "Music Midtown"];
-function getRandomArrayVal(array) { 
+
+var queryDict = {
+"Aquarium" : ["225 Baker St, Atlanta, GA 30313", "aquariums"],
+"Falcons" : ["1 AMB Dr, Atlanta, GA 30313", "sportsbars"],
+"Mercedes Benz stadium" : ["1 AMB Dr, Atlanta, GA 30313", "sportsbars"],
+"World of Coke" : ["121 Baker St NW, Atlanta, GA 30313", "museums"],
+"World of Coca Cola" : ["121 Baker St NW, Atlanta, GA 30313", "museums"],
+"Centennial Olympic Park" : ["265 Park Ave NW, Atlanta, GA 30313", "parks"],
+"Botanical Gardens" : ["1345 Piedmont Ave NE, Atlanta, GA 30309", "gardens"],
+"Zoo" : ["800 Cherokee Ave SE, Atlanta, GA 30315", "zoos"],
+"High Museum of Art" : ["1280 Peachtree St NE, Atlanta, GA 30309", "artmuseums"],
+"CNN Center" : ["190 Marietta St NW, Atlanta, GA 30303", "landmarks"],
+"Fernbank Museum" : ["767 Clifton Rd NE, Atlanta, GA 30307", "museums"],
+"Six Flags" : ["275 Riverside Pkwy SW, Austell, GA 30168", "amusementparks"],
+"Fox Theatre" : ["660 Peachtree St NE, Atlanta, GA 30308", "theater"],
+"Netherworld" : ["1313 Netherworld Way, Stone Mountain, GA 30087", "hauntedhouses"],
+"Music Midtown" : ["400 Park Dr, Atlanta, GA 30306", "festivals"]
+}
+
+function getRandomArrayVal(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 let sinceInfo = "since:2019-10-01";
@@ -63,14 +91,37 @@ function postSearch() {
             focusPost.userId = data.statuses[0].user.id;
             focusPost.postId = data.statuses[0].id;
             focusPost.postIdStr = data.statuses[0].id_str;
-            replyToPost();
+            replyToPost(focusPost);
         }
         updateGetParams();
     });
 }
 
-function replyToPost() {
+function replyToPost(focusPost) {
     //This is where Amadia uses the updated focusPost object to reply to the post.
+    searchRequest["categories"] = queryDict[focusPost.attraction][1];
+    //searchRequest["location"] = queryDict[focusPost.attraction][0];
+    YelpAPIKey.search(searchRequest).then(response => {
+        let result = response.jsonBody.businesses;
+        let num = Math.floor(Math.random()*Math.min(9, result.length - 1));
+        let recName = result[num]["name"];
+        let sender = "@" + focusPost.userId;
+        let tweetText = " If you are interested in " + focusPost.attraction + ", then you might also be interested in " + recName + "!";
+
+        if(debug) {
+            console.log('Debug mode: ', tweetText);
+        } else {
+            T.post('statuses/update', {in_reply_to_status_id: focusPost.postId, status: sender + tweetText}, function (err, reply) {
+                if (err != null){
+                    console.log('Error: ', err);
+                } else {
+                    console.log('Tweeted: ', tweetText);
+                }
+            });
+        }
+    }).catch(e => {
+        console.log(e);
+    });
 }
 
 //implement code.
